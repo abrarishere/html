281a350025a985231fd26e58b056350e6d560ded
Muhammad Abrar <153655587+abrarishere@users.noreply.github.com>
2024-01-27 08:48:31 +0000

    js work

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 })
+/*Aside */
+const nav = document.querySelector(".nav"),
+    navList = document.querySelector("li"),
+    totalNavList = navList.length;
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        for (let j = 0; j < totalNavList; j++) {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        // for (let j = 0; j < totalNavList; j++) {
+        //     if (navList[j].querySelector("a").classList.contains("active")) {
+        //         navList[j].querySelector("a").classList.remove("active");
+        //     }
+        // }
+        this.classList.add("active");
+        showSection(this);
+
+    })
+}
