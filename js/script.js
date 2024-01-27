65f679e6983603da3b24d2015179dc13cad69215
Muhammad Abrar <153655587+abrarishere@users.noreply.github.com>
2024-01-27 10:58:27 +0000

    1

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+/*Aside */
+const nav = document.querySelector(".nav"),
+    navList = document.querySelector("li"),
+    totalNavList = navList.length;
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        for (let j = 0; j < totalNavList; j++) {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        // for (let j = 0; j < totalNavList; j++) {
+        //     if (navList[j].querySelector("a").classList.contains("active")) {
+        //         navList[j].querySelector("a").classList.remove("active");
+        //     }
+        // }
+        this.classList.add("active");
+        showSection(this);
+    })
+}
+function showSection(element) {
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active");
+}
